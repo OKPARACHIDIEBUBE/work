d994450150a5703c2a44453b6b0b50e7d1684302
OKPARACHIDIEBUBE <rokeebede@gmail.com>
2023-11-27 02:48:07 -0800

    My first commit

---
+++ js/javascript.js
@@ -0,0 +1,265 @@
+
+
+// let num1 = Number(prompt('enter number1'))
+// let num2 = Number(prompt('enter number2'))
+// let num3 = Number(prompt('enter number3'))
+
+//     console.log(num1)
+//     console.log(num2)
+//     console.log(num3)
+
+// if(num1 > num2 && num1>num3){
+    
+//         if(num1 > num3){
+
+//             console.log(num1 +' is the biggest')
+// //     }else{
+
+// //             console.log(num3 +' is the biggest')
+// //     }
+// // }else if(num2 > num3 && num2 >num1){
+
+// //     console.log(num2 +' is the biggest')
+
+// // }else{
+
+// //     console.log(num3 +' is the biggest')
+
+
+
+
+// // let username = prompt('enter username')
+// // let password = Number(prompt('enter password')) 
+
+// // if (username == 'admin'&& password ==1234){
+
+// //     console.log(' logged in')
+
+// // }else if (username== 'admin' && password !==1234){
+
+// //     console.log('incorrect password')
+
+// // }else if(username !== 'admin' && password ==1234){
+
+// //     console.log('incorrect username')
+
+// // }else{
+
+// //     console.log('worng details')
+// // // }
+
+
+
+// // let a = (PerformanceObserver)
+// // let b = (12)
+// // let c =(9)
+
+// // console.log(a = 9)
+// // console.log(b =10)
+// // console.log(c =12)
+
+// // let num1 = Number(prompt('enter number1'))
+// // let num2 = Number(prompt('enter number2'))
+// // let num3 = Number(prompt('enter number3'))
+
+
+// let namesofFrontend=['Emeka','uche', 'prince']
+// let namesofBackend=['Emmanuel','james','ifeanyi']
+
+// let allstudent = namesofFrontend.concat(namesofBackend)
+
+// let student = prompt('enter student to find')
+// let found = allstudent.includes(student)
+
+// if(found){
+
+//     console.log(`the student ${student}was found`)
+
+// }else
+
+//     console.log(`the student ${student}was Not found`)
+
+
+// let listofname = allstudent.join('|')
+// console.log(listofname)
+
+// console.log(namesofBackend)
+// console.log(namesofFrontend)
+// console.log(months)
+
+let months = ["January", "February", "Monday", "Tues"]
+let days = months.splice(2, 2, "March", "April");
+console.log(months)
+
+
+
+const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+const citrus = fruits.slice(1, 3)
+console.log(fruits);
+
+
+// fruits.sort("Banana", "Orange", "Lemon", "Apple", "Mango");
+// console.log(fruits)
+
+// fruits.reverse();
+
+
+const point = [40, 100, 1, 5, 25, 10];
+point.sort(function(a,b){return a-b});
+    
+console.log[40, 100, 1, 5, 25, 10];
+console.log(function(a,b){return a-b});
+console.log(point)
+
+// const okpara={
+//     firstname:'okpara',
+//     lastname:'ebube',
+//     Age:'24',
+//     Gender:'male',
+//     class:'ss 3',
+//     hobbies:['singing','rapping'],
+//     cat1:'20',
+//     cat2:'30',
+//     exam:'80',
+// }
+// console.log(okpara)
+// const uche={
+//     firstname:'okpara',
+//     lastname:'chidiebube',
+//     Age:'23',
+//     Gender:'male',
+//     class:'ss 3',
+//     hobbies:['singing','rapping'],
+//     cat1:'20',
+//     cat2:'30',
+//     exam:'87',
+// }
+// console.log(uche)
+// const ugo={
+//     firstname:'okpara',
+//     lastname:'chidiebube',
+//     Age:'25',
+//     Gender:'male',
+//     class:'ss 3',
+//     hobbies:['singing','rapping'],
+//     cat1:'20',
+//     cat2:'30',
+//     exam:'80',
+// }
+// console.log(ugo)
+
+// const student = {
+//     firstname:'ugo',
+//     lastname:'chidi',
+//     Age:'25',
+//     Gender:'male',
+//     class:'ss 3',
+//     hobbies:['singing','rapping'],
+//     cat1:'20',
+//     cat2:'30',
+//     exam:'80',
+// }
+// console.log(student)
+// const sandra = {
+//     firstname:'ugo',
+//     lastname:'chidi',
+//     Age:'25',
+//     Gender:'male',
+//     class:'ss 3',
+//     hobbies:['singing','rapping'],
+//     cat1:'20',
+//     cat2:'30',
+//     exam:'80',
+// }
+// console.log(sandra)
+
+
+
+// let i = 5;
+// for (let i = 0; i < 40; i++){
+//     console.log(i)
+// }
+
+// let raduis = 8;
+
+// let p1 = 3.142;
+
+// let area = p1 * raduis **2;
+// console.log(area)
+
+// let circumference = 2 *p1*raduis;
+// console.log(circumference)
+
+
+// let principal = 100000
+// let time = 2
+// let rate = 10
+
+// let interest =(100000*10*2/100)
+
+// console.log(interest)
+
+// let cat1 = Number(prompt('Enter Cat 1 '))
+// let cat2 = Number(prompt('Enter Cat 2 '))
+// let exam = Number(prompt('Enter Cat exam '))
+
+// let total = cat1 + cat2 + exam
+// console.log(total)
+
+// if(total >= 70){
+//   console.log(" You passed your grade is A");
+// }else if (total >= 60){
+//   console.log(" You passed your grade is B");
+// }else if (total >= 50){
+//   console.log(" You passed your grade is C");
+// }else{
+//   console.log(" You passed your grade is F");
+// }
+
+
+// const names = ['emma', 'chidi', 'ebube', 'ebuka', 'nsugbe']
+// // console.log(names[0])
+// let total = cat1 + cat2 + exam
+
+// let firstname = (prompt('Enter firstname '))
+// let lastname = (prompt('Enter lastname'))
+// let middlename = (prompt('Enter middlename'))
+
+
+
+// const names = ['ike','ife','dela' ]
+
+// names.push('mama')
+// names.unshift('mandela')
+
+// console.log(names)
+
+
+
+// const bestplayer = ['haland','salah','sake','rashford','palmer']
+// bestplayer[3] = 'maddison'
+
+// bestplayer.push('son','watkins','diaby','fernandez')
+
+// const worstplayer = ['rashford','Anthony','hojlund','fernandez']
+
+// const allplayer = bestplayer.concat (worstplayer);
+
+// let player = prompt("enter players name")
+
+// console.log(allplayer)
+
+
+
+// let count = 1
+
+// while (count <=5){
+//     console.log(count)
+//     count++
+// }
+
+let count = 1
+while (count <=20){
+    console.log(count)
+    count++
+}
